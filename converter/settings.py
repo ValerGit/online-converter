ca3f1f3c20d57d91ab7ac1a61a35eacbafd262ef
Alex <digwnews@gmail.com>
2016-01-27 16:17:10 +0300

    db settings

--- converter/settings.py
+++ converter/settings.py
@@ -76,10 +76,14 @@ WSGI_APPLICATION = 'converter.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'convertdb',
+        'USER': 'devuser',
+        'PASSWORD': '123456',
+        'HOST': '127.0.0.1'
     }
 }
+
 
 
 # Internationalization
